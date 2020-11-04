77c18fad472b9eb8d9c93b0720da757036185c9e
fanyushun <fanyushun@meichai.in>
2020-11-04 14:45:10 +0800

    feat: 去除不需要的 eslint-disable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,9 +11,7 @@ import { getListData } from '@/services/list';
 export default (props: any): React.ReactNode => {
   const [original, setOriginal] = useState(!!(props.location.query.original === 'true'));
   const [order, setOrder] = useState(props.location.query.order * 1 || 0);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [page, setPage] = useState(props.location.query.page * 1 || 1);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [count, setCount] = useState(props.location.query.count * 1 || 10);
 
   const [listData, setListData] = useState<API.ListItemData[]>([]);
