f336c7e6106938e7aec9eb7845ee7754af827a23
fanyushun <fanyushun@meichai.in>
2020-12-17 10:49:17 +0800

    feat: 逻辑调整

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
 import ListAll from '@/components/List/ListAll';
-// import styles from './Home.less';
 
 export default (): React.ReactNode => <ListAll />;
--- src/services/list.ts
+++ src/services/list.ts
@@ -1,11 +1,21 @@
 import { request } from 'umi';
 
-export async function getListData(data: {
+export interface getListDataParamsTs {
+  // 每页数据条数
   count?: number;
+  // 第几页
   page?: number;
+  // 近看原创
   justOriginal?: boolean;
+  // 文章排序
   order?: number;
-}) {
+  // 博客专栏id
+  columnId?: number | string;
+  // 博客归档月份
+  dateTime?: string;
+}
+
+export async function getListData(data: getListDataParamsTs) {
   return request<API.ListDataRequest>('/articleList', {
     method: 'get',
     params: data,
